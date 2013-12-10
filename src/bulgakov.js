a3c904d78b5108a7ccd3547dfac97df217460e88
Seth Bonnie <sethkbonnie@gmail.com>
2013-12-10 03:17:20 -0600

    Cross-browser class remove/add

--- src/bulgakov.js
+++ src/bulgakov.js
@@ -1,12 +1,19 @@
 (function() {
-  var bkv_trigger = document.querySelector('.bkv_menu_trigger');
-  var bkv_menu = document.querySelector('.bkv_menu');
+  var get_trigger = function() {
+    document.querySelector('.bkv_menu_trigger');
+  };
+  var get_menu = function() {
+    return document.querySelector('.bkv_menu');
+  };
   var bkv_expanded = false;
 
   bkv = typeof bkv !== 'undefined'? bkv : {};
+  
   bkv.init = function() {
+    var bkv_menu = get_menu();
+    var bkv_trigger = get_trigger();
     if (bkv_menu)
-      bkv_menu.classList.add('contract');
+      bkv.addClass(bkv_menu, 'contract');
 
     if (bkv_trigger) {
       bkv_trigger.onclick = function() {
@@ -18,20 +25,67 @@
       };
     }
   };
+  
   bkv.expand = function() {
+    var bkv_menu = get_menu();
     bkv_expanded = true;
     if (bkv_menu) {
-      bkv_menu.classList.remove('contract');
-      bkv_menu.classList.add('expand');
+      bkv.removeClass(bkv_menu, 'contract');
+      bkv.addClass(bkv_menu, 'expand');
     }
   };
+  
   bkv.contract = function() {
+    var bkv_menu = get_menu();
     bkv_expanded = false;
     if (bkv_menu) {
-      bkv_menu.classList.remove('expand');
-      bkv_menu.classList.add('contract');
+      bkv.removeClass(bkv_menu, 'expand');
+      bkv.addClass(bkv_menu, 'contract');
     }
   };
+
+  // Class Helpers
+  bkv.addClass = function(el, cl) {
+    if (typeof document.body.classList !== "undefined") {
+      for (var i = 1, len = arguments.length; i < len; i++) {
+        el.classList.add(arguments[i].trim());
+      }
+
+    } else {
+      var classList = el.className.split(' ').filter(isNot(''));
+
+      for (var i = 1, len = arguments.length; i < len; i++) {
+        var className = arguments[i];
+        // Only append className if it's not already on the list
+        if (classList.indexOf(className) == -1)
+          el.className +=  ' ' + arguments[i].trim();
+      }
+    }
+  };
+
+  bkv.removeClass = function(el, cl) {
+    if (typeof document.body.classList !== "undefined") {
+      // Iterate through args
+      for (var i = 1, len = arguments.length; i < len; i++) {
+        el.classList.remove(arguments[i].trim());
+      }
+
+    } else {
+      var classList = el.className.split(' ').filter(isNot(''));
+
+      // Filter out the classname(s) supplied in the args
+      for (var i = 1, len = arguments.length; i < len; i++) {
+        var className = arguments[i].trim();
+        classList = classList.filter(isNot(className));
+      }
+
+      el.className = classList.join(' ');
+    }
+  };
+
+  // Private Methods
+  function isNot(x) { return function(el) { return el !== x; }; }
+  function is(x)    { return function(el) { return el === x; }; }
 
   bkv.init();
 })();
